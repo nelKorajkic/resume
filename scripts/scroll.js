4e0fc267afa43b5cd595e76a33a3a7ed6fcc85e6
Nel Korajkic <nelkorajkic@gmail.com>
2017-01-06 02:09:00 -0500

    fixed some silly javascript include error because jade is hard

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,5 +1,5 @@
 
 $('a[href*=#]').on('click', function(event){
     event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    $('html,body').animate({scrollTop:$(this.hash).offset().top+1000}, 500);
 });
