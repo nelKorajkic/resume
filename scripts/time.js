fcaf1ab57f26baf788cecc85d0acc80af7f8a3fd
Nel Korajkic <nelkorajkic@gmail.com>
2017-01-06 19:50:19 -0500

    Still trying to figure out the print pop-up dissappear on click, also gonna do the contact part differently for some wow factor :D

--- scripts/time.js
+++ scripts/time.js
@@ -2,13 +2,13 @@ var date = new Date();
 var time = date.getHours();
 console.log(time);
 function timecheck(){
-  if (time >= 5 && time <= 11) {
-  document.getElementById("personalScroll").innerHTML = "Good Morning";
+  if (time >= 5 & time <= 11) {
+  document.getElementById("personalScroll").innerHTML = 'Good Morning';
   }
-  else if (time >= 12 && time <= 7) {
-  document.getElementById("personalScroll").innerHTML = "Good Evening";
+  else if (time >= 12 & time <= 23) {
+  document.getElementById("personalScroll").innerHTML = 'Good Evening';
   }
-  else if (time >= 1 & time <= 3) {
+  else if (time >= 0 & time <= 3) {
   document.getElementById("personalScroll").innerHTML = 'Still up?' ;
 }}
 window.onload =timecheck;
